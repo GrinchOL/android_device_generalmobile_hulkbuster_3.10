b35b69060ceec93e043ce0293ddd779c7d1512d3
Kutlay SARIMEHMET <kutlaysarimehmet@gmail.com>
2018-01-11 15:40:42 +0300

    fix little

--- patches/apply.sh
+++ patches/apply.sh
@@ -2,6 +2,6 @@
 
 cd ../..
 cd apps/Settings
-patch -p1 -b < ../../../device/Lava/IrisX8/patches/dev_info.patch
+patch -p1 -b < ../../../device/generalmobile/hulkbuster/patches/dev_info.patch
 
 echo You Did It! Manish_4586!
